dfd9e548d0b3c402befc4fb621d4303325942aad
QuaRang1225 <dbduddnd1225@gmail.com>
2022-06-29 15:15:04 +0900

    2022/06/29

--- EX_SWIFT_1/EX_SWIFT.playground/Pages/EX_Controller.xcplaygroundpage/Contents.swift
+++ EX_SWIFT_1/EX_SWIFT.playground/Pages/EX_Controller.xcplaygroundpage/Contents.swift
@@ -4,6 +4,8 @@ import Foundation
 //조건문
 //if문
 var grade = 4     //자료형 생략 가능
+var boy:Bool = true
+
 
 if grade > 3 && grade <= 6{     //연산자와 숫자를 붙여놓으면 오류가 남
     print("고학년입니다")
@@ -14,6 +16,13 @@ else if grade >= 1 && grade <= 3{
 else {
     print("초등학생이 아닌데요..?")
 }
+if boy {    //true일 경우 간단식 false일경우 !boy
+    print("남자입니다")
+}else{
+    print("여자입니다")
+}
+var gender : Void = !boy ? print("남자입니다") : print("여자입니다")
+//삼항 연산자로 바로 표현 가능
 
 //switch문
 switch grade {      //a..<b는 a와 b사이의 있는 모든수를 뜻함
@@ -27,16 +36,34 @@ default :
 
 //반보문
 //for문
+
+//0...5
+//0,1,2,3,4,5
+
+//0..<5
+//0,1,2,3,4
+
 let i:Int
 let j:Int
 
-for i in 0..<6{             //i를 굳이 사용하지 않고 단순반복문만 사용하고 싶으면 _를 i 대신에 써줄 수 있다.
-    for j in 0..<i{
+for i in 1...5{             //i를 굳이 사용하지 않고 단순반복문만 사용하고 싶으면 _를 i 대신에 써줄 수 있다.
+    for j in 1...i{
         print("*",terminator: "")
     }
     print("")
 }
+var ranInt:[Int] = [Int]()
+for _ in 0..<25{
+    let ranNum = Int.random(in: 0...100)        //랜덤수를 생성하여 프로퍼티에 저장
+    ranInt.append(ranNum)           //배열에 랜덤 프로퍼티값을 저장
+}
+print("랜덤한 수 : ",ranInt)
 //for문 별찍기
+var arr : [Int] = [1,2,3,4,5,6,7,8,9,10]
+
+for i in arr where i % 2 == 0 {     //where이 붙으면 조건 첨가 가능
+    print("짝수 : \(i)")
+}
 
 //while문
 var a:Int = 0
@@ -45,6 +72,8 @@ while a < 10{
     a += 1
     print(a)
 }
+
+
 
 
 
---
+++ EX_SWIFT_1/EX_SWIFT.playground/Pages/EX_Enum.xcplaygroundpage/Contents.swift
@@ -0,0 +1,82 @@
+//: [Previous](@previous)
+
+import Foundation
+import CoreGraphics
+
+//enum(열거형)
+//관련된 값으로 이루어진 그룹을 공통의형으로 선언하여 안전성을 보장하는 방법
+
+enum Rainbow{           //스위프트의 열거형은 case별로 값을 할당하지 않아도 된다
+    case red,orange,yellow,green,blue,indigo,purple
+}
+var FavoriteColor = Rainbow.red
+FavoriteColor = .indigo     //위에서 선언 시 enum의 red로 최고하 했기 때문에 타입 추론으로 인하여 .indigo만 사용해도 가능
+print("가장좋아하는 색은 \(FavoriteColor)")
+
+enum Num:Int{
+    case first = 1,second = 2,thrid = 3
+}
+
+let ColorNum = Num.first.rawValue               //rawValue를 적어야 enum의 값을 출력할 수 있음
+print("\(FavoriteColor)는 \(ColorNum)번째 색껄이다")
+let color = Num(rawValue: 3)        //3을 가진 값을 저장 가능   //값이 없을 경우 nil이 저장됨
+print(color!)
+
+enum RainbowTranslation {       //함수형 enum
+    case red(name: String)
+    case orange(name: String)
+    case yellow(name: String)
+    case green(name: String)
+    case blue(name: String)
+    case indigo(name: String)
+    case purple(name: String)
+    
+    func color() {          //enum안에 함수 사용 가능
+        switch self{        //자기 자신의 맴버에 특정값이 들어올 경우
+        case .red(let name):
+            print(name)
+        case .orange(let name):
+            print(name)
+        case .yellow(let name):
+            print(name)
+        case .green(let name):
+            print(name)
+        case .blue(let name):
+            print(name)
+        case .indigo(let name):
+            print(name)
+        case let .purple(name):     //위랑 같은 경우
+            print(name)
+        }
+    }
+}
+
+let KoreanColor = RainbowTranslation.yellow(name : "노란색")   //yellow 맴버의 값 전달
+print(KoreanColor)
+KoreanColor.color()
+
+
+//재귀 열거자
+//다른 열거 인스턴스를 관계값으로 가지는 열거형
+
+enum Calculate {
+    case num(Int)
+    indirect case add(Calculate,Calculate)                  //만약 모든 열거형 맴버가 재귀열거자면 enum선언 때 indirect 사용 가능
+    indirect case division(Calculate,Calculate)             //ex? enum indirect calculate{}
+}
+let num1 = Calculate.num(6)
+let num2 = Calculate.num(4)
+let sum = Calculate.add(num1,num2)
+let result = Calculate.division(sum,Calculate.num(2))
+
+func Cal(_ operation:Calculate)->Int{           //_를 사용하면 맴버이름 사용안 할 수 있음
+    switch operation{
+    case let .num(value):
+        return value
+    case let .add(num1,num2):
+        return Cal(num1) + Cal(num2)
+    case let .division(num1,num2):
+        return Cal(num1) / Cal(num2)
+    }
+}
+print(Cal(result))     //재귀 열거 때문에 알아서 모든 case문을 거치고 연산된 값으로 나옴
