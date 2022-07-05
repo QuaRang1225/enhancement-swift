f2c720b7ba8331595c328395a5d247fe8f1f4638
QuaRang1225 <dbduddnd1225@gmail.com>
2022-07-05 20:13:27 +0900

    2022/07/05

--- EX_SWIFT_1/EX_SWIFT.playground/Pages/EX_Controller.xcplaygroundpage/Contents.swift
+++ EX_SWIFT_1/EX_SWIFT.playground/Pages/EX_Controller.xcplaygroundpage/Contents.swift
@@ -4,8 +4,6 @@ import Foundation
 //조건문
 //if문
 var grade = 4     //자료형 생략 가능
-var boy:Bool = true
-
 
 if grade > 3 && grade <= 6{     //연산자와 숫자를 붙여놓으면 오류가 남
     print("고학년입니다")
@@ -16,6 +14,9 @@ else if grade >= 1 && grade <= 3{
 else {
     print("초등학생이 아닌데요..?")
 }
+
+var boy:Bool = true
+
 if boy {    //true일 경우 간단식 false일경우 !boy
     print("남자입니다")
 }else{
@@ -45,7 +46,7 @@ default :
 
 let i:Int
 let j:Int
-
+//for문 별찍기
 for i in 1...5{             //i를 굳이 사용하지 않고 단순반복문만 사용하고 싶으면 _를 i 대신에 써줄 수 있다.
     for j in 1...i{
         print("*",terminator: "")
@@ -53,12 +54,13 @@ for i in 1...5{             //i를 굳이 사용하지 않고 단순반복문만
     print("")
 }
 var ranInt:[Int] = [Int]()
+
 for _ in 0..<25{
     let ranNum = Int.random(in: 0...100)        //랜덤수를 생성하여 프로퍼티에 저장
     ranInt.append(ranNum)           //배열에 랜덤 프로퍼티값을 저장
 }
 print("랜덤한 수 : ",ranInt)
-//for문 별찍기
+
 var arr : [Int] = [1,2,3,4,5,6,7,8,9,10]
 
 for i in arr where i % 2 == 0 {     //where이 붙으면 조건 첨가 가능
@@ -66,12 +68,7 @@ for i in arr where i % 2 == 0 {     //where이 붙으면 조건 첨가 가능
 }
 
 //while문
-var a:Int = 0
-
-while a < 10{
-    a += 1
-    print(a)
-}
+ 
 
 
 
---
+++ EX_SWIFT_1/EX_SWIFT.playground/Pages/EX_Optiional.xcplaygroundpage/Contents.swift
@@ -0,0 +1,62 @@
+
+import Foundation
+
+//옵셔넝 - 값이 존재할 수도 안할 수도 있는것
+
+var val:Int?    //보통은 초기화를 시켜야 하지만 옵셔널로 선언하면 초기화를 하지 않아도 에러가 발생하지 않음
+//초기화를 따로 하지 않으면 nil값이 저장됨
+val = 3
+print(val)      //옵셔널 변수이기 때문에 래핑된 상태로 출력 되게 됨
+print(val!)     //강제로 옵셔널을 벗길 수 있음(강제 언래핑)      //좋은 방법은 아님
+
+//옵셔널 바인딩
+if let unwrapVal = val{
+    print(unwrapVal)
+}
+
+val = nil
+let numVal = val ?? 30  //변수 안에 값이 nil일 경우 값을 넣어줌
+print(numVal)
+
+var fristVal:Int? = 11
+
+func unWrap(_ param: Int?){
+    guard let unwrapVal = param else{
+        return
+    }
+    print(unwrapVal)
+}
+unWrap(fristVal)
+
+
+//옵셔널 체이닝 - 옵셔널을 사슬처럼 연달아서 사용하는 것
+struct Information {
+    var grade : Int
+    var major : String
+}
+
+struct Student {
+    var name : String
+    var info : Information
+    
+    init(_ name : String, _ grade : Int, _ major : String) {
+        self.name = name
+        info = Information(grade  : grade, major : major)
+    }
+}
+var my : Student? = Student("콰랑", 1, "스위프트학과")
+//변수를 옵셔널로 선언한 후 구조체 저장
+
+//my = nil
+if let myname = my?.name, let myinfo = my?.info{
+    print("나는 \(myinfo.grade)학년 \(myinfo.major) \(myname)입니다.")
+}else{
+    print("존재하지 않는 학생입니다.")
+}
+
+
+
+
+
+
+
