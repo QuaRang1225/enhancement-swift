03513b4d2c58ca45389f39c4193ea1956c6c887f
QuaRang1225 <dbduddnd1225@gmail.com>
2023-12-25 23:25:49 +0900

    파일 삭제

--- test.playground/Pages/Untitled Page 2.xcplaygroundpage/Contents.swift
+++ test.playground/Pages/Untitled Page 2.xcplaygroundpage/Contents.swift
@@ -2,6 +2,23 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
-
-//: [Next](@next)
+class Count{
+    static let shared = Count()
+    var num = 0
+    func countNum(){
+        num += 1
+        print(num)
+    }
+}
+func call1(){
+    Count.shared.countNum()
+}
+func call2(){
+    Count.shared.countNum()
+}
+func call3(){
+    Count.shared.countNum()
+}
+call1()
+call2()
+call3()
