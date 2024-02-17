5337228d652acd6cbc040fc213aa0ecadb9bb3e3
QuaRang1225 <dbduddnd1225@gmail.com>
2024-02-17 18:00:17 +0900

    스택 코드 추가

--- DataStructure.playground/Contents.swift
+++ DataStructure.playground/Contents.swift
@@ -1,3 +1,30 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//MARK: 스택구조
+struct Stack<T>{
+    var stack:[T] = []
+    
+    var count:Int{
+        return stack.count
+    }
+    
+    var isEmpty:Bool{
+        return stack.isEmpty
+    }
+    mutating func push(_ value:T){
+        stack.append(value)
+    }
+    mutating func pop() -> T? {
+        return stack.isEmpty ? nil : stack.popLast()
+    }
+}
+
+var stack = Stack<Int>()
+print(stack.count)
+print(stack.isEmpty)
+print(stack.push(3))
+print(stack.stack)
+print(stack.push(6))
+print(stack.stack)
+print(stack.pop() ?? "배열 비어있음")
+
