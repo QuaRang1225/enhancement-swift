002c7b69084af13cd645f8c56cfa7b4601a28c9f
QuaRang1225 <dbduddnd1225@gmail.com>
2024-02-17 17:24:03 +0900

    합병 정렬 추가

--- Sort.playground/Contents.swift
+++ Sort.playground/Contents.swift
@@ -9,19 +9,19 @@ var sortedArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 //MARK: 버블정렬 알고리즘 시간복잡도 : O(N^2)
 func bubbleSort(_ array:inout [Int]){
     
-//    let n = array.count
-//    var isSwap = false
-//    guard n > 1 else {return}
-//    for i in 0..<(n-1){
-//        for j in 0..<(n-1 - i){
-//            if array[j] > array[j + 1]{
-//                array.swapAt(j, j+1)
-//                isSwap = true
-//                swapCount += 1
-//            }
-//        }
-//        guard !isSwap else {return}
-//    }
+    //    let n = array.count
+    //    var isSwap = false
+    //    guard n > 1 else {return}
+    //    for i in 0..<(n-1){
+    //        for j in 0..<(n-1 - i){
+    //            if array[j] > array[j + 1]{
+    //                array.swapAt(j, j+1)
+    //                isSwap = true
+    //                swapCount += 1
+    //            }
+    //        }
+    //        guard !isSwap else {return}
+    //    }
     let n = array.count
     var isSwap = false
     guard n > 1 else{ return }
@@ -45,18 +45,18 @@ func bubbleSort(_ array:inout [Int]){
 //MARK: 선택정렬 알고리즘 시간복잡도 : O(N^2)
 func selectSort(_ array:inout [Int]){
     
-//    let n = array.count
-//    guard n > 1 else {return}
-//    for i in 0..<(n-1){
-//        var min = i
-//        for j in (i+1)..<n{
-//            if array[min] > array[j]{
-//                min = j
-//            }
-//            swapCount += 1
-//        }
-//        array.swapAt(min, i)
-//    }
+    //    let n = array.count
+    //    guard n > 1 else {return}
+    //    for i in 0..<(n-1){
+    //        var min = i
+    //        for j in (i+1)..<n{
+    //            if array[min] > array[j]{
+    //                min = j
+    //            }
+    //            swapCount += 1
+    //        }
+    //        array.swapAt(min, i)
+    //    }
     let n = array.count
     guard n > 1 else {return}
     for i in 0..<n-1{
@@ -77,18 +77,18 @@ func selectSort(_ array:inout [Int]){
 
 //MARK: 삽입 정렬 알고리즘 시간복잡도 : O(N^2)
 func insertSrot(_ array:inout [Int]){
-//    let n = array.count
-//    guard n > 1 else {return}
-//    for i in 1..<n{
-//        for j in stride(from: i, to: 0, by: -1){
-//            if array[j] < array[j-1]{
-//                array.swapAt(j, j-1)
-//                swapCount += 1
-//            }else{
-//                break
-//            }
-//        }
-//    }
+    //    let n = array.count
+    //    guard n > 1 else {return}
+    //    for i in 1..<n{
+    //        for j in stride(from: i, to: 0, by: -1){
+    //            if array[j] < array[j-1]{
+    //                array.swapAt(j, j-1)
+    //                swapCount += 1
+    //            }else{
+    //                break
+    //            }
+    //        }
+    //    }
     let n = array.count
     guard n > 1 else {return}
     for i in 1..<n{
@@ -113,13 +113,67 @@ func factorial(_ value:Int) -> Int{
 //print(factorial(3))
 
 
-//MARK: 퀵정렬 시간복잡도 O(N) - 만약 피벗이 항상 배열중 가장 크거나 작을 경우 O(N^2)
+//MARK: 퀵정렬 시간복잡도 O(N logN) - 만약 피벗이 항상 배열중 가장 크거나 작을 경우 O(N^2)
 func quickSort(_ array: inout [Int]) -> [Int]{
     guard let first = array.first,array.count > 1 else {return array}
     
-    var previous = array.filter{$0 < first}
-    var after = array.filter{$0 > first}
+    var previous = [Int]()
+    var after = [Int]()
+    
+    array.enumerated().filter { $0.offset != 0 }
+        .forEach {
+            if $0.element > first {
+                after.append($0.element)
+            } else if $0.element <= first {
+                previous.append($0.element)
+            }
+        }
     return quickSort(&previous) + [first] + quickSort(&after)
 }
 
-print(quickSort(&array))
+//print(quickSort(&array))
+
+
+//MARK: 피보나치 수열 O(N)
+func fibo(_ value:Int) -> Int{
+    var temp = [0,1]
+    (2...value).forEach {
+        temp.append(temp[$0-1] + temp[$0-2])
+    }
+    return temp[value]
+}
+
+//print(fibo(7))
+
+func mergeSort(_ array:[Int]) -> [Int]{
+    guard array.count > 1 else {return array}
+    
+    let center = array.count/2
+    let left = Array(array[0..<center])
+    let right = Array(array[center..<array.count])
+    
+    func merge(_ left:[Int],_ right: [Int]) -> [Int] {
+        
+        var leftArr = left
+        var rightArr = right
+        var result: [Int] = []
+        
+        while !leftArr.isEmpty,!rightArr.isEmpty{
+            if leftArr[0] < rightArr[0]{
+                result.append(leftArr.removeFirst())
+            }else{
+                result.append(rightArr.removeFirst())
+            }
+        }
+        if !leftArr.isEmpty{
+            result.append(contentsOf: leftArr)
+        }
+        if !rightArr.isEmpty{
+            result.append(contentsOf: rightArr)
+        }
+        return result
+    }
+    return merge(mergeSort(left), mergeSort(right))
+}
+print(mergeSort(array))
+//print(1)
