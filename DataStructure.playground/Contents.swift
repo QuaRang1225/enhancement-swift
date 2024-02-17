fc790e8da5f544e8061798c47b5fa081207d2968
QuaRang1225 <dbduddnd1225@gmail.com>
2024-02-17 22:29:10 +0900

    링크드 리스트 노드 추가 코드

--- DataStructure.playground/Contents.swift
+++ DataStructure.playground/Contents.swift
@@ -75,5 +75,24 @@ class Node<T>{
     }
 }
 
-
+//MARK: 링크드 리스트 생성
+class LinkedList<T>{
+    var head:Node<T>?
+    
+    func append(_ data:T){
+        //헤더가 없을 경우 해당 노트가 헤더가 됨
+        if head == nil{
+            head = Node(data: data)
+            return
+        }
+        //검색을 하기 위해서 헤드부터 검색을 해야하기 때문에 노드에 헤드를 저장
+        var node = head
+        //헤더가 기존에 존재할경우 헤더를 검색해서 next가 nil인 노드를 검색하며 노드를 업데이트 하다가 찾으면 루프 탈출
+        while node?.next != nil {
+            node = node?.next
+        }
+        //node.next가 nil인게 확인 후 데이터를 추가
+        node?.next = Node(data: data)
+    }
+}
 
