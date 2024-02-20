8b264d2b5e70e51ff29613822d9f16049b28bf57
QuaRang1225 <dbduddnd1225@gmail.com>
2024-02-20 16:45:20 +0900

    이진탐색트리 다이어그램 프린트 코드 추가

--- DataStructure.playground/Contents.swift
+++ DataStructure.playground/Contents.swift
@@ -209,4 +209,18 @@ class BinarySearchTree<T:Comparable>{
             }
         }
     }
+    private func diagram(for node: BSTNode<T>?,
+                         _ top: String = "",
+                         _ root: String = "",
+                         _ bottom: String = "") -> String {
+       guard let node = node else {
+            return root + "nil\n"
+        }
+        if node.left == nil && node.right == nil {
+            return root + "\(node.data)\n"
+        }
+        return diagram(for: node.right, top + " ", top + "┌──", top + "│ ")
+            + root + "\(node.data)\n"
+            + diagram(for: node.left, bottom + "│ ", bottom + "└──", bottom + " ")
+    }
 }
